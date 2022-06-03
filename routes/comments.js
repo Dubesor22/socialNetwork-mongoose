a09d0b64e2efcf786977770b542669e0a668b550
Dubesor22 <dubesor22@gmail.com>
2022-06-04 01:48:18 +0200

    crud

--- routes/comments.js
+++ routes/comments.js
@@ -4,7 +4,7 @@ const CommentController = require("../controllers/CommentController");
 const { authentication, isAdmin } = require("../middlewares/authentication");
 
 router.post("/:_id", authentication, CommentController.create);
-router.put("/update/:_id", authentication, isAdmin, CommentController.update);
+router.put("/:_id", authentication, isAdmin, CommentController.update);
 router.get("/", CommentController.getAll);
 router.delete("/:_id", authentication, CommentController.delete)
 
