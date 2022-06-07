1e359eee687d5b7c5a87230296af6845b04efee0
Dubesor22 <dubesor22@gmail.com>
2022-06-07 13:57:27 +0200

    fixes

--- routes/users.js
+++ routes/users.js
@@ -5,15 +5,16 @@ const { authentication, isAdmin } = require("../middlewares/authentication");
 
 const upload = require("../middlewares/multer");
 
-  router.post("/", UserController.create);
-  router.get("/confirm/:emailToken", UserController.confirm);
-  router.post("/login", UserController.login);
-  router.put("/update", authentication, upload.single("avatar"), UserController.updateUser);
-  router.get("/",authentication, UserController.getUser);
-  router.put("/logout", authentication, UserController.logout);
-  router.put("/follow/:_id", authentication, UserController.follow);
-  router.put("/unfollow/:_id", authentication, UserController.removeFollow);
-  router.delete("/", authentication, UserController.deleteUser);
+router.post("/", UserController.create);
+router.get("/confirm/:emailToken", UserController.confirm);
+router.post("/login", UserController.login);
+router.put("/", authentication, upload.single("avatar"), UserController.updateUser);
+router.get("/",authentication, UserController.getUser);
+router.put("/logout", authentication, UserController.logout);
+router.put("/follow/:_id", authentication, UserController.follow);
+router.put("/unfollow/:_id", authentication, UserController.removeFollow);
+router.delete("/", authentication, UserController.deleteUser);
+
   router.delete(
       "/clean-all",
       authentication,
